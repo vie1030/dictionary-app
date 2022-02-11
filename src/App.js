69a5582fa955e31298a3b1a6c337b1068991fabe
vie1030 <91474368+vie1030@users.noreply.github.com>
2022-02-11 17:25:01 +0100

    set-up the dictionary search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary App Project</header>
+      <div className="container">
+        <header className="App-header">📚 Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This is GitHub open-sourced project coded by Vie Santos and hosted
+            on Netlify.com.
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
